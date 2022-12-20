76803c820bdce9f0c5c14a65d2f04ce6f283ab2a
Robert Kasper <roberthkasper@gmail.com>
2022-12-20 12:00:20 -0500

    Adds Vector2Multiply extensions + tests

---
+++ Runtime/Vector2Extensions/Vector2Multiply.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Vector2Extensions
+{
+	public static class Vector2Multiply
+	{
+		/// <summary>
+		/// Returns the component-wise product of two Vector2 objects
+		/// </summary>
+		public static Vector2 Multiply(this Vector2 v1, Vector2 v2) => new Vector2(v1.x * v2.x, v1.y * v2.y);
+		
+		/// <summary>
+		/// Returns the component-wise product of a Vector2 and a Vector2Int
+		/// </summary>
+		public static Vector2 Multiply(this Vector2 v1, Vector2Int v2) => new Vector2(v1.x * v2.x, v1.y * v2.y);
+	}
+}
